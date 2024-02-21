b1d9defc78ee028dc10b398aac59cbd66c0503fa
sirius <sirius2beta@gmail.com>
2024-02-21 15:39:03 +0800

    DNApplication create

---
+++ DenovoViewer/dnapplication.cpp
@@ -0,0 +1,18 @@
+﻿#include "dnapplication.h"
+
+DNApplication::DNApplication(int &argc, char *argv[])
+    :QApplication (argc, argv)
+{
+    QQmlApplicationEngine* qmlEngine = new QQmlApplicationEngine(this);
+    qmlEngine->load("qrc:/main.qml");
+}
+
+DNApplication::~DNApplication()
+{
+
+}
+
+void DNApplication::_shutdown()
+{
+
+}
---
+++ DenovoViewer/dnapplication.h
@@ -0,0 +1,18 @@
+﻿#ifndef DNAPPLICATION_H
+#define DNAPPLICATION_H
+
+#include <QApplication>
+#include <QQmlApplicationEngine>
+
+class DNApplication:public QApplication
+{
+    Q_OBJECT
+public:
+    DNApplication(int &argc, char* argv[]);
+    ~DNApplication();
+    void _shutdown();
+protected:
+
+};
+
+#endif // DNAPPLICATION_H
